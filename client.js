877f8a9ff5b584b68752aeb1e5876ad074713acc
Noor Abdilahi <noorkeey@yahoo.com>
2020-10-07 07:38:24 -0500

    added scripts, formating, styles

--- client.js
+++ client.js
@@ -1,18 +1,55 @@
 console.log('Here are all the available people:', people);
 
-$(document).ready(onReady);
+let randomPerson = {}
 
-function onReady() {
-  console.log('jquery is loaded');
-  for (people of people) {
-    $('#guessWho').append(`
-
-     <div>
-
-     <img src= "src="https://github.com/${people.githubUsername}.png?size=250" alt="Profile image of Norbu"></img>
-
-     </div>
-
-     `);
-  }
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function addClass(className, id) {
+	$(`#${id}`).addClass(className)
+
+	setTimeout(() => {
+		$(`#${id}`).removeClass(className)
+	}, 2000)
+}
+
+function updateGame() {
+	randomPerson = people[randomNumber(0, people.length - 1)]
+	$('#prompt').html(`Guess who? Pick ${randomPerson.name}`)
+	console.log('Random person = ' + randomPerson.name)
+}
+
+$(document).ready(function(){
+	updateGame()
+
+	people.forEach(person => {
+		let html = `<img id="${person.name}" src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">`
+		$('#images').append(html)
+	})
+
+	$('img').click(function() {
+		let id = $(this).attr('id')
+
+		if(id == randomPerson.name) {
+			//addClass('success', id)
+			alert('You guessed it right! Click OK to play again.')
+			updateGame()
+
+		} else {
+			//addClass('error', id)
+			alert('Ops! Try again.')
+		}
+
+	})
+});
